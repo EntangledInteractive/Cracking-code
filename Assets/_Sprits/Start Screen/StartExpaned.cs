c4c7d58753e6096d047856a177f65807ba4242d7
Nicolla Elkhouri <nicollae@entangleinteractive.com>
2017-06-01 12:14:00 +1000

    dix animations

---
+++ Assets/_Sprits/Start Screen/StartExpaned.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartExpaned : MonoBehaviour {
+    public Animator anim;
+    public AnimationClip startExp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PlayExpand()
+    {
+        anim.SetBool("Expand", true);
+        
+    }
+}
